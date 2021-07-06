a5015a406772dd2a9d54c04c423271207d734e64
User <user@MacBook-Pro-de-User.local>
2021-07-06 11:51:11 +0200

    ex_01

---
+++ ex_01/Animal.php
@@ -0,0 +1,12 @@
+<?php
+    class animals {
+    public function  __const() {
+        private $MAMMAL = 0;
+        private $FISH = 1;
+        private $BIRD = 2; 
+        private $type 
+        private $legs
+        private $name
+    }
+}
+?> 
